0eeb4daa400323afc569b858f0740c3411d6a592
Omkar Shirote <shiroteomkar6@gmail.com>
2024-02-19 12:28:34 +0530

    Power of Two

---
+++ Power of Two/Solution3.cpp
@@ -0,0 +1,11 @@
+// https://leetcode.com/problems/power-of-two/description/
+
+#include<bits/stdc++.h>
+using namespace std;
+    
+class Solution {
+public:
+    bool isPowerOfTwo(int n) {
+        return n > 0 && ((int)pow(2, 30) % n) == 0;
+    }
+};

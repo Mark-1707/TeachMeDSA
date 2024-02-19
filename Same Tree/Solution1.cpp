eb391404d49694d883f8f8ac0e3241b81f044391
Omkar Shirote <shiroteomkar6@gmail.com>
2024-02-19 22:27:29 +0530

    Subtree of Another Tree

---
+++ Same Tree/Solution1.cpp
@@ -0,0 +1,37 @@
+// https://leetcode.com/problems/same-tree/description/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+
+    bool check(TreeNode* p, TreeNode* q){
+        if(p->val == q->val) return true;
+        return false;
+    }
+
+    bool isSameTree(TreeNode* p, TreeNode* q) {
+        
+        if(p == NULL && q == NULL) return true;
+        if(p == NULL && q != NULL) return false;
+        if(p != NULL && q == NULL) return false;
+
+        bool n = check(p, q);
+        if(!n) return false;
+
+        return isSameTree(p->left, q->left) && isSameTree(p->right, q->right);
+
+    }
+};

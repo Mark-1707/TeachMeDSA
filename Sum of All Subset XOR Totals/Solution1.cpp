363c0c1373943963486cc78f5021a6a7092182f7
Omkar Shirote <shiroteomkar6@gmail.com>
2024-05-21 00:33:21 +0530

    Sum of All Subset XOR Totals

---
+++ Sum of All Subset XOR Totals/Solution1.cpp
@@ -0,0 +1,37 @@
+// https://leetcode.com/problems/sum-of-all-subset-xor-totals
+
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+
+private:
+    vector<int> set;
+    int ans = 0;
+    void findAns(int i,  vector<int>& nums){
+
+        if(i >= nums.size()){
+            int xo = 0;
+            for(int& n: set){
+                xo  n;
+            }
+            ans += xo;
+            return;
+        }
+
+        set.push_back(nums[i]);
+
+        findAns(i + 1, nums);
+
+        set.pop_back();
+
+        findAns(i + 1, nums);
+
+    }
+
+public:
+    int subsetXORSum(vector<int>& nums) {
+        findAns(0, nums);
+        return ans;
+    }
+};
